b90c1ad7f11254c8dba5166b1d08b472bfd00494
Stefan Seifert <nine@detonation.org>
2010-01-29 19:55:28 +0100

    Version 0.32

--- Python.pm
+++ Python.pm
@@ -6,7 +6,7 @@ require DynaLoader;
 require Exporter;
 use vars qw(@ISA $VERSION @EXPORT_OK);
 @ISA = qw(Inline DynaLoader Exporter);
-$VERSION = '0.31';
+$VERSION = '0.32';
 @EXPORT_OK = qw(py_eval
 		py_new_object
 		py_call_method 
