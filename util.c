92ff95cb77b39f36d8726ead828290489ac3b27f
Stefan Seifert <nine@detonation.org>
2012-06-19 12:59:33 +0200

    const vars wherever possible in util.c

--- util.c
+++ util.c
@@ -17,7 +17,7 @@ extern "C" {
 int free_inline_py_obj(pTHX_ SV* obj, MAGIC *mg)
 {
   if (mg && mg->mg_type == PERL_MAGIC_ext && Inline_Magic_Check(mg->mg_ptr)) {
-    IV iv = SvIV(obj);
+    IV const iv = SvIV(obj);
     /*Printf(("free_inline_py_obj: %p, iv: %p, ob_prev: %p, ob_next: %p, refcnt: %i\n", obj, iv, ((PyObject *)iv)->_ob_prev, ((PyObject *)iv)->_ob_next, ((PyObject *)iv)->ob_refcnt)); */ /* _ob_prev and _ob_next are only available if Python is compiled with reference debugging enabled */
     Printf(("free_inline_py_obj: %p, iv: %p, refcnt: %i\n", obj, iv, ((PyObject *)iv)->ob_refcnt));
     free(mg->mg_virtual); /* allocated in Py2Pl */
@@ -31,17 +31,17 @@ int free_inline_py_obj(pTHX_ SV* obj, MAGIC *mg)
 }
 
 PyObject * get_perl_pkg_subs(PyObject *package) {
-  char *pkg = PyString_AsString(package);
-  PyObject *retval = PyList_New(0);
-  HV *hash = perl_get_hv(pkg, 0);
-  int len = hv_iterinit(hash);
+  char * const pkg = PyString_AsString(package);
+  PyObject * const retval = PyList_New(0);
+  HV * const hash = perl_get_hv(pkg, 0);
+  int const len = hv_iterinit(hash);
   int i;
 
   for (i=0; i<len; i++) {
-    HE *next = hv_iternext(hash);
+    HE * const next = hv_iternext(hash);
     I32 n_a;
-    char *key = hv_iterkey(next,&n_a);
-    char *test = (char*)malloc((strlen(pkg) + strlen(key) + 1)*sizeof(char));
+    char * const key = hv_iterkey(next,&n_a);
+    char * const test = (char*)malloc((strlen(pkg) + strlen(key) + 1)*sizeof(char));
     sprintf(test,"%s%s",pkg,key);
     if (perl_get_cv(test,0)) {
       PyList_Append(retval, PyString_FromString(key));
@@ -55,8 +55,8 @@ PyObject * get_perl_pkg_subs(PyObject *package) {
 int perl_pkg_exists(char *base, char *pkg) {
   int retval = 0;
 
-  HV* hash = perl_get_hv(base,0);
-  char *fpkg = (char*)malloc((strlen(pkg) + strlen("::") + 1)*sizeof(char));
+  HV * const hash = perl_get_hv(base,0);
+  char * const fpkg = (char*)malloc((strlen(pkg) + strlen("::") + 1)*sizeof(char));
   sprintf(fpkg,"%s::",pkg);
 
   Printf(("perl_pkg_exists: %s, %s --> %s\n", base, pkg, fpkg));
@@ -72,11 +72,11 @@ int perl_pkg_exists(char *base, char *pkg) {
 }
 
 PyObject * perl_sub_exists(PyObject *package, PyObject *usub) {
-  char *pkg = PyString_AsString(package);
-  char *sub = PyString_AsString(usub);
-  PyObject *retval = Py_None;
+  char * const pkg = PyString_AsString(package);
+  char * const sub = PyString_AsString(usub);
+  PyObject * retval = Py_None;
 
-  char *qsub = (char*)malloc((strlen(pkg) + strlen(sub) + 1)*sizeof(char));
+  char * const qsub = (char*)malloc((strlen(pkg) + strlen(sub) + 1)*sizeof(char));
   sprintf(qsub,"%s%s",pkg,sub);
   
   if (perl_get_cv(qsub,0)) {
@@ -91,7 +91,7 @@ PyObject * perl_sub_exists(PyObject *package, PyObject *usub) {
 
 int py_is_tuple(SV *arr) {
   if (SvROK(arr) && SvTYPE(SvRV(arr)) == SVt_PVAV) {
-    MAGIC *mg = mg_find(SvRV(arr), PERL_MAGIC_ext);
+    MAGIC * const mg = mg_find(SvRV(arr), PERL_MAGIC_ext);
     return (mg && Inline_Magic_Key(mg->mg_ptr) == TUPLE_MAGIC_KEY);
   }
   else
